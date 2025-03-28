9b1b473cabfc101f853120e1e8423b9cca30b388
Raju Ghimire <raju.805421@cct.tu.edu.np>
2025-03-28 10:00:59 +0545

    Movies rendered with flatlist 'only title'

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,26 +2,57 @@ import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import SearchBar from "@/components/SearchBar";
 import { useRouter } from "expo-router";
-import { Text, View, Image, ScrollView } from "react-native";
+import { Text, View, Image, ActivityIndicator, FlatList } from "react-native";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
 
+export default function Index() {
+  const router = useRouter();
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: "" }));
 
-
-export default function Index(){
-  const router=useRouter();
   return (
-    <View className="bg-primary flex-1 ">
+    <View className="bg-primary flex-1">
       <Image source={images.bg} className="absolute w-full z-0" />
 
-      <ScrollView
-        className="flex-1 px-5"
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
-      >
-        <Image source={icons.logo} className="mx-auto mt-20 mb-10" />
-        <View className="flex-1 mt-5">
-        <SearchBar onPress={()=>{router.push("/search")}} placeholder="Search for a movie . . ."/>
-        </View>
-      </ScrollView>
+      {/* Show Loading or Error */}
+      {moviesLoading ? (
+        <ActivityIndicator size="large" color="#0000ff" className="mt-10 self-center" />
+      ) : moviesError ? (
+        <Text>Error: {moviesError?.message}</Text>
+      ) : (
+        <FlatList
+          data={movies}
+          keyExtractor={(item) => item.id.toString()}
+          numColumns={3}
+          columnWrapperStyle={{
+            justifyContent: "flex-start",
+            gap: 20,
+            paddingRight: 5,
+            marginBottom: 10,
+          }}
+          ListHeaderComponent={
+            <>
+              <Image source={icons.logo} className="mx-auto mt-20 mb-10" />
+              <SearchBar
+                onPress={() => router.push("/search")}
+                placeholder="Search for a movie . . ."
+              />
+              <Text className="text-lg text-white font-bold mt-5 mb-3">
+                Latest Movies
+              </Text>
+            </>
+          }
+          renderItem={({ item }) => (
+            <Text className="text-white text-sm">{item.title}</Text>
+          )}
+          contentContainerStyle={{ paddingHorizontal: 20, paddingBottom: 32 }}
+          showsVerticalScrollIndicator={false}
+        />
+      )}
     </View>
   );
 }

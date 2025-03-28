41386eabf6cff35080e7eae41f2167f4774701e6
Raju Ghimire <raju.805421@cct.tu.edu.np>
2025-03-28 10:44:11 +0545

    Movie Card design complete - good to see

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,9 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
+import { images } from '../constants/images';
+import star from '@/assets/icons/star.png';
+import { icons } from "@/constants/icons";
 
 const MovieCard = ({
   id,
@@ -11,7 +14,7 @@ const MovieCard = ({
 }: Movie) => {
     console.log(poster_path)
   return (
-    <Link href={`/movie/${id}`} asChild>
+    <Link href={`/movies/${id}`} asChild>
       <TouchableOpacity className="w-[30%]">
         <Image
           source={{
@@ -22,7 +25,16 @@ const MovieCard = ({
           className="w-full h-52 rounded-lg"
           resizeMode="cover"
         />
-        <Text className="text-white">{title}</Text>
+        <Text className="text-white text-sm font-bold mt-2" numberOfLines={1}>{title}</Text>
+        <View className="flex-row items-center">
+            <Image source={icons.star} className="size-4 mr-1"/>
+            <Text className="text-white text-sm uppercase font-bold">{Math.round(vote_average/2)}</Text>
+        </View>
+        <View className="flex-row items-center justify-between">
+            <Text className="text-light-300 font-medium mt-1">
+                {release_date?.split('-')[0]}
+            </Text>
+        </View>
       </TouchableOpacity>
     </Link>
   );

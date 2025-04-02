b9d9856ffc684f99d9bc7170c324ce869b99acb5
Raju Ghimire <raju.805421@cct.tu.edu.np>
2025-04-02 15:59:58 +0545

    Movies card layout design  with flatlist properties & movieslogo inserted

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,12 +1,14 @@
-import { View, Text ,Image,FlatList} from 'react-native'
-import React from 'react'
-import { images } from '@/constants/images'
-import MovieCard from '@/components/MovieCard'
-import { useRouter } from 'expo-router'
-import useFetch from '@/services/useFetch'
-import { fetchMovies } from '@/services/api'
+import { View, Text, Image, FlatList } from "react-native";
+import React from "react";
+import { images } from "@/constants/images";
+import MovieCard from "@/components/MovieCard";
+import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
 
-const search = () => {
+const Search = () => {
   const router = useRouter();
   const {
     data: movies,
@@ -16,10 +18,35 @@ const search = () => {
 
   return (
     <View className="bg-primary flex-1">
-      <Image source={images.bg} className="flex-1 absolute w-full z-0" resizeMode='cover'/>
-      <FlatList data={movies} renderItem={({item})=><MovieCard {...item}/>}/>
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      />
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        className="px-5"
+        numColumns={3}
+        columnWrapperStyle={{
+          gap: 5,
+          justifyContent: "center",
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20 items-center">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+          </>
+        }
+      />
     </View>
-  )
-}
+  );
+};
 
-export default search
+export default Search;
